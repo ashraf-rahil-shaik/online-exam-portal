a82f4bd747a202cd81db73c2e14f567ba7cad445
Ashraf-Rahil <skashraf97@gmail.com>
2023-02-01 17:52:34 +0530

    added nav

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import Login from './Login';
+import './App.css';
+// import Signup from './Signup';
+
+function App() {
+  return (
+    <div className="App">
+     <a href='/'> <h1 className="App-header">Online Exam Portal </h1></a>
+     <ul className='buttons'>   
+   
+      <button className='login'>Login</button>
+       <button className='signup'>Signup</button>
+       </ul>
+   
+    </div>
+  );
+}
+
+export default App;
+
