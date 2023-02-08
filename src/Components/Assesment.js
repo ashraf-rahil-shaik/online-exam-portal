c25fffeb2d6c061b03b5747786210cf70d9c523f
ashraf-rahil-shaik <skashraf97@gmail.com>
2023-02-08 16:52:57 +0530

    added babelrc

--- src/Components/Assesment.js
+++ src/Components/Assesment.js
@@ -9,6 +9,7 @@ const Card = ({ name, time,trainingType ,Stream,Pricing,vacancies}) => (
     <p className="card-time">Stream: {Stream}</p>
     <p className="card-time">Pricing:{Pricing}</p>
     <p className="card-time">vacancies: {vacancies}</p>
+   
   </div>
 );
 
@@ -16,25 +17,21 @@ const AssessmentPage = () => {
   const programmingLanguages = [
     { name: 'C',
      Duration : "80 days" ,
-
      trainingType : 'vertiual',
-    
     Stream : "Information Technology",
-    
     Pricing : "3000/-",
-    
     vacancies :"600",
-    
     LastDateToApply : "31st March 2023"},
     { name: 'C++', time: '40 mins' },
     { name: 'Java', time: '30 mins' },
-  
+   
   ];
 
   return (
     <div className="container">
-      {programmingLanguages.map(({ name, Duration,trainingType,Stream,Pricing,vacancies}) => (
-        <Card key={name} name={name} time={Duration}  trainingType ={trainingType}  Stream={Stream} Pricing={Pricing} vacancies={vacancies}/>
+      {programmingLanguages.map((programmingLanguages) => (
+
+        <Card key={programmingLanguages.name} {...programmingLanguages}/>
       ))}
     </div>
   );
